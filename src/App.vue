b33468f0d175ef7498a1c6352497e1b8f1d7f371
garnet <garnet_xin@foxmail.com>
2017-03-07 21:54:48 +0800

    update the draft for the todo list

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,46 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <h1 v-text="title"></h1>
+    <input v-model="newItem" v-on:keyup.enter="addNew"/>
+    <ul>
+      <li v-for="item in items" @click="toggleFinished(item)" v-bind:class="{finished: item.isFinished}">
+        {{item.label}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import Store from './store'
+console.log(Store);
 export default {
-  name: 'app'
+  data: function(){
+    return{
+      title: "this is a todo list",
+      items:Store.fetch(),
+      newItem: ''
+    }
+  },
+  watch:{
+    items:{
+      handler:function(items){
+        Store.save(items)
+      },
+      deep: true
+    }
+  },
+  methods: {
+    toggleFinished: function(item){
+      item.isFinished = ! item.isFinished;
+    },
+    addNew: function(){
+      this.items.push({
+        label: this.newItem,
+        isFinished: false
+      })
+      this.newItem = ""
+    }
+  }
 }
 </script>
 
@@ -19,5 +52,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.finished{
+  text-decoration: underline;
 }
 </style>
